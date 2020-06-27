00225555f2caef9a9d7f40188fe9528ed1146936
lollykrown <joe_kayu@yahoo.com>
2020-06-27 02:02:15 +0100

    .

--- main.js
+++ main.js
@@ -77,20 +77,18 @@ const coronavirus = ["Please stay home or wear face mask if you must go out and 
 const covid = ["Please stay home or wear face mask if you must go out maintain social distance"];
 const bye = ["Thank you for using lollykrown's Bot"];
 
-const submit = document.getElementById("send")
+const submit = document.getElementById("send");
 document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.getElementById("input")
   inputField.addEventListener("keyup", function (e) {
-   
+
     if (e.code === "Enter" || e.code == 13) {
-      submit.addEventListener("click", ()=>{
-        event.preventDefault();
-      // document.getElementById("send").addEventListener("submit", function (e) {
-      //   e.preventDefault();
-      let input = inputField.value;
-      console.log(input)
-      inputField.value = "";
-      output(input);
+      submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        let input = inputField.value;
+        console.log(input)
+        inputField.value = "";
+        output(input);
       })
     }
     // }
